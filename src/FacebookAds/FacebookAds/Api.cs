a2fa32751d75f5639101051e2db02056b1b46ce3
Bert Zwier <bert@traffic-builders.com>
2020-09-28 13:45:06 +0200

    .NET Library: converted to .NET Core for automatic inclusion of conversion output - Optimized API class - Removed converter output - Included solution file

--- src/FacebookAds/FacebookAds/Api.cs
+++ src/FacebookAds/FacebookAds/Api.cs
@@ -1,116 +1,34 @@
 ﻿using Facebook;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-/// <summary>
-/// The MIT License (MIT)
-///
-/// Copyright (c) 2016 - Luke Paris (Paradoxis) | Searchresult Performancemarketing
-///
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-///
-/// The above copyright notice and this permission notice shall be included in all
-/// copies or substantial portions of the Software.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-/// SOFTWARE.
-/// </summary>
 namespace FacebookAds
 {
     /// <summary>
-    /// Api class initializer
+    ///     Api class initializer
     /// </summary>
-    public class Api
+    public static class Api
     {
         /// <summary>
-        /// The version of the graph API we're using
+        ///     The version of the graph API we're using
         /// </summary>
-        public const string VERSION = "v2.11";
+        private const string VERSION = "v5.0";
 
         /// <summary>
-        /// The client
-        /// </summary>
-        protected static FacebookClient client = null;
-
-        /// <summary>
-        /// Gets or sets the client.
-        /// </summary>
-        /// <value>
-        /// The client.
-        /// </value>
-        public static FacebookClient Client
-        {
-            get
-            {
-                return Api.client;
-            }
-
-            set
-            {
-                Api.client = value;
-            }
-        }
-
-        /// <summary>
-        /// Initializes the application via pre-initialized clients
-        /// </summary>
-        /// <param name="client">The client.</param>
-        public static void Initialize(FacebookClient client)
-        {
-            Api.client = client;
-        }
-
-        /// <summary>
-        /// Initializes the application via tokens
+        ///     Initializes the application via tokens
         /// </summary>
         /// <param name="appId">The application identifier.</param>
         /// <param name="appSecret">The application secret.</param>
         /// <param name="accessToken">The access token.</param>
-        public static void Initialize(string appId, string appSecret, string accessToken, string apiVersion = null)
+        /// <param name="apiVersion">The API version.</param>
+        public static FacebookClient Initialize(string appId, string appSecret, string accessToken,
+            string apiVersion = null)
         {
-            Api.client = new FacebookClient();
-            Api.client.AppId = appId;
-            Api.client.AppSecret = appSecret;
-            Api.client.AccessToken = accessToken;
-            Api.client.Version = apiVersion != null ? apiVersion : Api.VERSION;
-        }
-
-        /// <summary>
-        /// Destroys the client object and resets the API
-        /// Might as well just re-initialize the object instead, but added for consistency.
-        /// </summary>
-        public static void Destroy()
-        {
-            Api.client = null;
-        }
-
-        /// <summary>
-        /// Checks if the class is initialized
-        /// </summary>
-        public static bool IsInitialized
-        {
-            get
+            return new FacebookClient
             {
-                return (Api.client != null);
-            }
+                AppId = appId,
+                AppSecret = appSecret,
+                AccessToken = accessToken,
+                Version = apiVersion ?? VERSION
+            };
         }
-
-        /// <summary>
-        /// Prevents a default instance of the <see cref="Api"/> class from being created.
-        /// </summary>
-        private Api() { }
     }
 }
--- src/FacebookAds/FacebookAds/Interfaces/AbstractCrudObject.cs
+++ src/FacebookAds/FacebookAds/Interfaces/AbstractCrudObject.cs
@@ -1,234 +1,189 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Collections.Specialized;
-
 using Facebook;
 using Newtonsoft.Json.Linq;
 
-/// <summary>
-/// The MIT License (MIT)
-///
-/// Copyright (c) 2016 - Luke Paris (Paradoxis) | Searchresult Performancemarketing
-///
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-///
-/// The above copyright notice and this permission notice shall be included in all
-/// copies or substantial portions of the Software.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-/// SOFTWARE.
-/// </summary>
 namespace FacebookAds.Interfaces
 {
     /// <summary>
-    /// Abstract create-remote-update-delete Object interface
+    ///     Abstract create-remote-update-delete Object interface
     /// </summary>
     public abstract class AbstractCrudObject
     {
         /// <summary>
-        /// The ID field identifier
+        ///     The ID field identifier
         /// </summary>
-        public const string FIELD_ID = "id";
+        private const string FieldId = "id";
 
         /// <summary>
-        /// The FIELDS field identifier
+        ///     The FIELDS field identifier
         /// </summary>
-        public const string FIELD_FIELDS = "fields";
+        private const string FieldFields = "fields";
 
         /// <summary>
-        /// The identifier
+        ///     The data
         /// </summary>
-        protected string id;
+        private readonly Dictionary<string, object> _data = new Dictionary<string, object>();
 
         /// <summary>
-        /// The parent identifier
+        ///     The identifier
         /// </summary>
-        protected string parentId;
+        private readonly string _id;
 
         /// <summary>
-        /// The data
+        ///     The parent identifier
         /// </summary>
-        protected Dictionary<string, object> data = new Dictionary<string, object>();
+        private readonly string _parentId;
 
         /// <summary>
-        /// The client
+        ///     Initializes a new instance of the <see cref="AbstractCrudObject" /> class.
         /// </summary>
-        protected FacebookClient client;
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="AbstractCrudObject" /> class.
-        /// </summary>
+        /// <param name="client">The client.</param>
         /// <param name="id">The identifier.</param>
         /// <param name="parentId">The parent identifier.</param>
-        /// <param name="client">The client.</param>
-        /// <exception cref="FacebookApiException">An Api instance must be provided as argument or set as instance in [FacebookAds.Api]</exception>
-        public AbstractCrudObject(string id, string parentId = null, FacebookClient client = null)
+        /// <exception cref="FacebookApiException">
+        ///     An Api instance must be provided as argument or set as instance in
+        ///     [FacebookAds.Api]
+        /// </exception>
+        protected AbstractCrudObject(FacebookClient client, string id, string parentId = null)
         {
             // Set the ID
-            if (id != null) {
-                this.id = id;
-                data.Add(FIELD_ID, id); // causing LOTS of conflicts with URL's
+            if (id != null)
+            {
+                _id = id;
+                _data.Add(FieldId, id); // causing LOTS of conflicts with URL's
             }
 
             // Set the parent ID
-            if (parentId != null) {
-                this.parentId = id;
-            }
+            if (parentId != null) _parentId = id;
 
             // Check if the client is set
-            if (client != null) {
-                this.client = client;
-            } else {
-                if (Api.IsInitialized) {
-                    this.client = Api.Client;
-                } else {
-                    throw new FacebookApiException("An Api instance must be provided as argument or set as instance in [FacebookAds.Api]");
-                }
-            }
+            if (client != null)
+                Client = client;
+            else
+                throw new FacebookApiException(
+                    "An Api instance must be provided as argument or set as instance in [FacebookAds.Api]");
         }
 
         /// <summary>
-        /// Gets the client.
+        ///     Gets the client.
         /// </summary>
         /// <value>
-        /// The client.
+        ///     The client.
         /// </value>
-        protected FacebookClient Client
-        {
-            get
-            {
-                return client;
-            }
-        }
+        private FacebookClient Client { get; }
 
         /// <summary>
-        /// Assures the endpoint is valid.
+        ///     Assures the endpoint is valid.
         /// </summary>
         /// <param name="endpoint">The endpoint.</param>
         /// <returns></returns>
-        private string assureEndpoint(string endpoint = null)
+        private string AssureEndpoint(string endpoint = null)
         {
             // Check if endpoint is empty
-            if (endpoint == null || endpoint == "") {
-                endpoint = this.GetEndpoint();
+            if (!string.IsNullOrEmpty(endpoint)) return endpoint;
+            endpoint = GetEndpoint();
 
-                // Endpoint still empty?
-                if (endpoint == null || endpoint == "") {
-                    throw new FacebookApiException("Endpoint must be provided via a parameter or GetEndpoint()");
-                } else {
-                    return endpoint;
-                }
-            } else {
-                return endpoint;
-            }
+            // Endpoint still empty?
+            if (string.IsNullOrEmpty(endpoint))
+                throw new FacebookApiException("Endpoint must be provided via a parameter or GetEndpoint()");
+            return endpoint;
         }
 
         /// <summary>
-        /// Assures that the endpoint string is valid via a Type reference.
+        ///     Assures that the endpoint string is valid via a Type reference.
         /// </summary>
         /// <param name="reference">The reference.</param>
         /// <returns></returns>
         /// <exception cref="FacebookApiException">Endpoint reference type must be provided via a parameter</exception>
-        private string assureEndpoint(Type reference)
+        private string AssureEndpoint(Type reference)
         {
-            if (reference == null) {
+            if (reference == null)
                 throw new FacebookApiException("Endpoint reference type must be provided via a parameter");
-            } else {
-                AbstractCrudObject obj = (AbstractCrudObject) Activator.CreateInstance(reference, new object[] { id, parentId, Client });
-                return obj.GetEndpoint();
-            }
+
+            var obj = (AbstractCrudObject) Activator.CreateInstance(reference, _id, _parentId, Client);
+            return obj.GetEndpoint();
         }
 
         /// <summary>
-        /// Prepares the data.
+        ///     Prepares the data.
         /// </summary>
         /// <param name="fields">The fields.</param>
         /// <param name="parameters">The parameters.</param>
         /// <returns></returns>
-        private Dictionary<string, object> assureData(string[] fields = null, Dictionary<string, object> parameters = null)
+        private Dictionary<string, object> AssureData(string[] fields = null,
+            Dictionary<string, object> parameters = null)
         {
-            if (fields != null) {
-                data.Add(FIELD_FIELDS, String.Join(",", fields));
-            } else {
-                data.Add(FIELD_FIELDS, "");
-            }
+            _data.Add(FieldFields, fields != null ? string.Join(",", fields) : "");
 
-            if (parameters != null) {
-                foreach (KeyValuePair<string, object> entry in parameters) {
-                    data.Add(entry.Key, entry.Value);
-                }
-            }
+            if (parameters == null) return _data;
+            foreach (var entry in parameters)
+                _data.Add(entry.Key, entry.Value);
 
-            return data;
+            return _data;
         }
 
         /// <summary>
-        /// Gets many objects by connection without a specified endpoint
+        ///     Gets many objects by connection without a specified endpoint
         /// </summary>
+        /// <param name="reference"></param>
         /// <param name="fields">The fields.</param>
         /// <param name="parameters">The parameters.</param>
         /// <returns></returns>
-        protected object getManyByConnection(Type reference, string[] fields = null, Dictionary<string, object> parameters = null)
+        protected object GetManyByConnection(Type reference, string[] fields = null,
+            Dictionary<string, object> parameters = null)
         {
-            return JObject.Parse(Client.Get(this.assureEndpoint(reference), this.assureData(fields, parameters)).ToString());
+            return JObject.Parse(Client.Get(AssureEndpoint(reference), AssureData(fields, parameters)).ToString());
         }
 
         /// <summary>
-        /// Gets many objects by connection.
+        ///     Gets many objects by connection.
         /// </summary>
         /// <param name="endpoint">The endpoint.</param>
         /// <param name="fields">The fields.</param>
         /// <param name="parameters">The parameters.</param>
         /// <returns>
-        /// Result of Client.Get
+        ///     Result of Client.Get
         /// </returns>
-        protected object getManyByConnection(string endpoint, string[] fields = null, Dictionary<string, object> parameters = null)
+        protected object GetManyByConnection(string endpoint, string[] fields = null,
+            Dictionary<string, object> parameters = null)
         {
-            return JObject.Parse(Client.Get(this.assureEndpoint(endpoint), this.assureData(fields, parameters)).ToString());
+            return JObject.Parse(Client.Get(AssureEndpoint(endpoint), AssureData(fields, parameters)).ToString());
         }
 
         /// <summary>
-        /// Gets the many by connection asynchronous.
+        ///     Gets the many by connection asynchronous.
         /// </summary>
         /// <param name="reference">The reference.</param>
         /// <param name="fields">The fields.</param>
         /// <param name="parameters">The parameters.</param>
         /// <returns>Result of Client.GetTaskAsync</returns>
-        protected object getManyByConnectionAsync(Type reference, string[] fields = null, Dictionary<string, object> parameters = null)
+        protected object GetManyByConnectionAsync(Type reference, string[] fields = null,
+            Dictionary<string, object> parameters = null)
         {
-            return JObject.Parse(Client.GetTaskAsync(this.assureEndpoint(reference), this.assureData(fields, parameters)).ToString());
+            return JObject.Parse(Client.GetTaskAsync(AssureEndpoint(reference), AssureData(fields, parameters))
+                .ToString());
         }
 
         /// <summary>
-        /// Gets many objects by connection asynchronously.
+        ///     Gets many objects by connection asynchronously.
         /// </summary>
         /// <param name="endpoint">The endpoint.</param>
         /// <param name="fields">The fields.</param>
         /// <param name="parameters">The parameters.</param>
         /// <returns>Result of Client.GetTaskAsync</returns>
-        protected object getManyByConnectionAsync(string endpoint, string[] fields = null, Dictionary<string, object> parameters = null)
+        protected object GetManyByConnectionAsync(string endpoint, string[] fields = null,
+            Dictionary<string, object> parameters = null)
         {
-            return JObject.Parse(Client.GetTaskAsync(this.assureEndpoint(endpoint), this.assureData(fields, parameters)).ToString());
+            return JObject.Parse(Client.GetTaskAsync(AssureEndpoint(endpoint), AssureData(fields, parameters))
+                .ToString());
         }
 
         /// <summary>
-        /// Gets the endpoint of the API call.
+        ///     Gets the endpoint of the API call.
         /// </summary>
         /// <returns>
-        /// Endpoint URL
+        ///     Endpoint URL
         /// </returns>
         protected abstract string GetEndpoint();
     }
